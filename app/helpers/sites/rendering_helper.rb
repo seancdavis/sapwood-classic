2b212b21737b4a9f74369ea45adade6e326848a0
Sean C Davis <scdavis41@gmail.com>
2014-10-22 07:00:07 -0400

    add new user button

--- app/helpers/sites/rendering_helper.rb
+++ app/helpers/sites/rendering_helper.rb
@@ -1,6 +1,14 @@
 module Sites
   module RenderingHelper
 
+    def page_title(title, &block)
+      content_tag(:section, :class => 'page-title') do
+        o = content_tag(:h1, title)
+        o += capture(&block) if block_given?
+        o.html_safe
+      end
+    end
+
     def data_table(headers, &block)
       content_tag(:table) do
         a = content_tag(:thead) do
