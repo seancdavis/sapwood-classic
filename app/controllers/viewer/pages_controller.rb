6509a1a1d1334754142f0718aa35e271dedfd037
Sean C Davis <scdavis41@gmail.com>
2014-10-28 19:01:03 -0400

    add index viewer with rocktree as an example

--- app/controllers/viewer/pages_controller.rb
+++ app/controllers/viewer/pages_controller.rb
@@ -1,5 +1,11 @@
 class Viewer::PagesController < ViewerController
 
+  def index
+    @page = current_page_type; not_found if @page.nil?
+    view = "viewer/#{current_site.slug}/#{current_page_type.slug}/index"
+    render view, :layout => "#{current_site.slug}"
+  end
+
   def show
     @page = current_page; not_found if @page.nil?
     view = "viewer/#{current_site.slug}/#{current_page_type.slug}/show"
