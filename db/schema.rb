3f515cb2b33da6a4c30ca0ec514294422e89b0e8
Sean C Davis <scdavis41@gmail.com>
2014-10-29 07:46:20 -0400

    add profile data to users

--- db/schema.rb
+++ db/schema.rb
@@ -114,12 +114,16 @@ ActiveRecord::Schema.define(version: 20141029111505) do
   end
 
   create_table "heartwood_users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.integer  "account_id"
+    t.string   "name"
+    t.text     "settings"
+    t.boolean  "is_admin",               default: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
