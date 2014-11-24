74c26076a545e965cb3b9075dd247f13cf45b2de
Sean C Davis <scdavis41@gmail.com>
2014-11-23 19:58:55 -0500

    content for home page

--- app/services/transform_u_fitness.rb
+++ app/services/transform_u_fitness.rb
@@ -4,8 +4,12 @@ class TransformUFitness
     @site = site
   end
 
-  def results
-    @site.page_types.find_by_slug('result').pages
+  def results(limit = nil)
+    if limit.nil?
+      @site.page_types.find_by_slug('result').pages
+    else
+      @site.page_types.find_by_slug('result').pages.limit(limit)
+    end
   end
 
   def free_consultation_form
