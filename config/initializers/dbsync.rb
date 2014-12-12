dc04f98af4264adbadb92fc1f2a46177287b8692
Sean C Davis <scdavis41@gmail.com>
2014-12-12 15:31:03 -0500

    only load dbsync in development

--- config/initializers/dbsync.rb
+++ config/initializers/dbsync.rb
@@ -1,4 +1,8 @@
-Dbsync.file_config = {
-  :remote => TaprootSetting.remote.db_backup_file,
-  :local  => TaprootDatabase.new.backup_symlink
-}
+if Rails.env.development?
+
+  Dbsync.file_config = {
+    :remote => TaprootSetting.remote.db_backup_file,
+    :local  => TaprootDatabase.new.backup_symlink
+  }
+
+end
