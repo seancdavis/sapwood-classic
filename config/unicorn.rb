29674446cfe694c80e50a1977445189f3abcab7e
Sean C Davis <scdavis41@gmail.com>
2014-12-12 15:54:52 -0500

    add unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory   "/home/deploy/apps/taproot"
+pid                 "/home/deploy/apps/taproot/tmp/pids/unicorn.pid"
+stderr_path         "/home/deploy/apps/taproot/log/unicorn.log"
+stdout_path         "/home/deploy/apps/taproot/log/unicorn.log"
+
+listen              "/tmp/unicorn.taproot.sock"
+worker_processes    2
+timeout             30
