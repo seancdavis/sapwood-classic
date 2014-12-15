81eb952577c56c160cbba49ac4b67256c54c15f6
Sean C Davis <scdavis41@gmail.com>
2014-12-15 09:54:35 -0500

    fix issue with figuring out current site

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -2,7 +2,7 @@ module SitesHelper
 
   def current_site
     @current_site ||= begin
-      if ['localhost','cms.rocktree.us'].include?(request.host)
+      if ['localhost',TaprootSetting.site.url].include?(request.host)
         p = params[:site_slug] || params[:slug]
         if user_signed_in?
           my_sites.select{ |s| s.slug == p }.first unless p.nil?
