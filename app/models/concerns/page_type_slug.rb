bbd1cf051b802921faf43a9272c5a7a99117d519
Sean C Davis <scdavis41@gmail.com>
2015-01-02 15:41:40 -0500

    fix page type slug dup logic

--- app/models/concerns/page_type_slug.rb
+++ app/models/concerns/page_type_slug.rb
@@ -14,18 +14,20 @@ module PageTypeSlug
   end
 
   def create_slug
-    association = self.class.table_name.gsub(/^heartwood\_/, '')
     slug = clean_slug(self.title.downcase.gsub(/\&/, ' and '))
-    dups = self.page_type.send(association).where(:slug => slug) - [self]
-    slug = "#{slug}-#{self.id}" if dups.count > 0
-    slug
   end
 
   def clean_slug(s)
     clean_slug = s.gsub(/[^a-zA-Z0-9 \-\_]/, "") # remove all bad characters
     clean_slug.gsub!(/\ /, "_") # replace spaces with underscores
     clean_slug.gsub!(/\_+/, "_") # replace repeating underscores
-    clean_slug
+    uniquify_slug(clean_slug)
+  end
+
+  def uniquify_slug(s)
+    dups = self.page_type.send(self.class.table_name).where(:slug => s) - [self]
+    s = "#{slug}-#{self.id}" if dups.count > 0
+    s
   end
 
   def to_param
