270cf03f8bdeb578617593c3c6d931795a02b14f
Sean C Davis <scdavis41@gmail.com>
2015-02-06 06:00:47 -0500

    allow for passing multiple arguments to a builder route

--- app/helpers/routing_helper.rb
+++ app/helpers/routing_helper.rb
@@ -19,19 +19,16 @@ module RoutingHelper
       ts = t.singularize; s += "#{ts}_"
       item == items.last ? i += "#{t}_" : i += "#{ts}_"
     end
-    objs = items.reverse.drop(1).reverse
-    objs = objs.collect(&:to_param).join(',')
+    objs = objs = items.reverse.drop(1).reverse
     case action
     when :index
-      objs.empty? ? send("#{i}path") : send("#{i}path", objs)
+      objs.empty? ? send("#{i}path") : send("#{i}path", *objs)
     when :new
-      objs.empty? ? send("new_#{s}path") : send("new_#{s}path", objs)
+      objs.empty? ? send("new_#{s}path") : send("new_#{s}path", *objs)
     when :edit
-      objs.empty? ? send("edit_#{s}path", items.last.to_param) : 
-        send("edit_#{s}path", objs, items.last.to_param)
+      send("edit_#{s}path", *items)
     when :show
-      objs.empty? ? send("#{s}path", items.last.to_param) : 
-        send("#{s}path", objs, items.last.to_param)
+      send("#{s}path", *items)
     end
   end
 
