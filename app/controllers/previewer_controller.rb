5589f4ff0beae7b9439931778282d8c6c3f9023c
Sean C Davis <scdavis41@gmail.com>
2015-02-14 09:34:55 -0500

    fixes #92 -- open up preview to public

--- app/controllers/previewer_controller.rb
+++ app/controllers/previewer_controller.rb
@@ -1,6 +1,6 @@
 class PreviewerController < Viewer::PagesController
 
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
 
   def dashboard
     redirect_to builder_sites_path
