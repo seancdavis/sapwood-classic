504538f9c27881ee974910b682fb9031df9f0969
Warren Harrison <wharrison@topicdesign.com>
2015-04-10 09:40:36 -0400

    Include project-specific schedule.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
 every 1.day, :at => '3:00 am' do
   rake "sitemap:refresh"
 end
+# Load project schedules
+Dir.glob("#{Rails.root}/projects/*/utilities/schedule.rb").each |file|
+  require file
+end
