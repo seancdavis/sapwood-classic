8dba4b4551e05eb3c1ca3f07aeacdeb24816a486
Sean C Davis <scdavis41@gmail.com>
2015-04-28 19:56:23 -0400

    add password and host to pg_dump

--- app/services/sapwood_database.rb
+++ app/services/sapwood_database.rb
@@ -7,7 +7,11 @@ class SapwoodDatabase
   end
 
   def backup
-    system("pg_dump --username #{config[:user]} #{config[:name]} > #{backup_file}")
+    cmd  = "PGPASSWORD=\"#{config[:password]}\" pg_dump"
+    cmd += " --username #{config[:user]} --host #{config[:host]}"
+    cmd += " #{config[:name]} > #{backup_file}"
+    puts cmd
+    system(cmd)
     if File.exists?(backup_file_copy)
       system("rm #{backup_file_copy}")
     end
@@ -23,12 +27,16 @@ class SapwoodDatabase
     def config
       @config ||= begin
         db_info = YAML.load_file(config_file)
-        {
+        config = {
           :adapter  => db_info[Rails.env]['adapter'],
           :name     => db_info[Rails.env]['database'],
           :user     => db_info[Rails.env]['username'],
-          :password => db_info[Rails.env]['password']
+          :password => db_info[Rails.env]['password'],
         }
+        unless db_info[Rails.env]['host'].blank?
+          config[:host] = db_info[Rails.env]['host']
+        end
+        config
       end
     end
 
