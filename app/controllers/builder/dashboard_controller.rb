0a25c4805b31d7e87f7d97ec898a40c265919d40
Sean C Davis <scdavis41@gmail.com>
2015-05-20 07:05:14 -0400

    toggle header color when inside a site

--- app/controllers/builder/dashboard_controller.rb
+++ app/controllers/builder/dashboard_controller.rb
@@ -4,6 +4,7 @@ class Builder::DashboardController < BuilderController
     if !current_user.admin? && !has_multiple_sites?
       redirect_to(builder_site_path(only_site))
     end
+    @options['body_classes'] = 'dashboard'
   end
 
 end
