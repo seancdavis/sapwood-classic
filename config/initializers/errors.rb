1d02645354efe5ba89eaea5e61b49b5c1258c573
Sean C Davis <scdavis41@gmail.com>
2015-07-28 11:58:19 -0400

    update errors initializer

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -2,9 +2,9 @@ if Rails.env.production?
   Rails.application.config.middleware.use(
     ExceptionNotification::Rack,
     :email => {
-      :email_prefix => SapwoodSetting.notifications.errors.email_prefix,
-      :sender_address => [SapwoodSetting.notifications.errors.sender],
-      :exception_recipients => [SapwoodSetting.notifications.errors.recipient]
+      :email_prefix => SapwoodSetting.errors.email_prefix,
+      :sender_address => [SapwoodSetting.errors.sender],
+      :exception_recipients => [SapwoodSetting.errors.recipient]
     }
   )
 end
