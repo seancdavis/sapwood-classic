5521524fef7ffe11b2dc59bf4a2ea658b290c904
Sean C Davis <scdavis41@gmail.com>
2015-07-28 11:51:02 -0400

    change settings file

--- config/initializers/_settings.rb
+++ config/initializers/_settings.rb
@@ -1,11 +1,7 @@
 begin
-  config_file = File.join(Rails.root,'config','sapwood.yml')
-  SapwoodSetting = YAML.load_file(config_file)[Rails.env].to_ostruct
+  config_file = File.join(Rails.root,'config','topkit.yml')
+  SapwoodSetting = {}
+  TopkitSetting = YAML.load_file(config_file)[Rails.env].to_ostruct
 rescue
-  begin
-    backup_config = File.join(Rails.root,'config','taproot.yml')
-    SapwoodSetting = YAML.load_file(backup_config)[Rails.env].to_ostruct
-  rescue
-    raise "Can't find file: #{config_file}"
-  end
+  raise "Can't find file: #{config_file}"
 end
