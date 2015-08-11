5a6caba2db02dbc290a6fb75417ac9063cafcb24
Sean C Davis <scdavis41@gmail.com>
2015-08-11 12:46:54 -0400

    clean up page presenter

--- app/presenters/page_presenter.rb
+++ app/presenters/page_presenter.rb
@@ -8,14 +8,4 @@ class PagePresenter
     @obj.published? ? 'published' : 'draft'
   end
 
-  # def first_name
-  #   return '' if @obj.name.nil?
-  #   @obj.name.split(' ').first
-  # end
-
-  # def last_name
-  #   return '' if @obj.name.nil?
-  #   @obj.name.split(' ').last
-  # end
-
 end
