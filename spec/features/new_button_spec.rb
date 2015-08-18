9f84d23cfee4092b10229833d4ea437793025a62
Sean C Davis <scdavis41@gmail.com>
2015-08-18 15:56:04 -0400

    remove git schmonz from feature spec

--- spec/features/new_button_spec.rb
+++ spec/features/new_button_spec.rb
@@ -4,13 +4,7 @@ feature 'New button' do
 
   before :all do
     @user = create(:user, :password => 'password')
-    @site = Site.find_or_create_by(
-      :title => 'Hello World 123',
-      :slug => 'hello-world-123',
-      :git_url => 'git@github.com:topicdesign/topkit-test-template.git',
-      :uid => '69b0386a13b44503881d516a2c19cc4a2bf48974d552a397'
-    )
-    system("cd #{Rails.root}/projects && git clone #{@site.git_url} #{@site.slug}")
+    @site = Site.find_or_create_by(:title => 'Hello World 123')
     create(:site_user, :site => @site, :user => @user)
   end
 
