2ac8c0e0d2a8aeeaa194397f0fe1a620be5b121c
Sean C Davis <scdavis41@gmail.com>
2015-08-28 11:10:29 -0400

    can delete blocks

--- app/controllers/editor/blocks_controller.rb
+++ app/controllers/editor/blocks_controller.rb
@@ -1,2 +1,10 @@
 class Editor::BlocksController < Editor::BaseController
+
+  def destroy
+    block_page = find_page(params[:slug])
+    block = Block.find_by(:page => current_page, :block => block_page)
+    block.destroy
+    redirect_to redirect_route, :notice => 'Block removed'
+  end
+
 end
