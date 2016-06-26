7d74ab4374507d790a5b9de6076f8e70f3277d4a
Sean C Davis <scdavis41@gmail.com>
2016-06-26 16:21:50 -0400

    add resources to site export (#313)

--- app/services/export_site.rb
+++ app/services/export_site.rb
@@ -2,7 +2,7 @@ class ExportSite
 
   def initialize(options = {})
     @site = options[:site]
-    @templates = @site.templates.includes(:template_fields, :webpages => [:template])
+    @templates = @site.templates.includes(:template_fields, :webpages => [:template, :resources => [:resource_type]])
   end
 
   def call
@@ -10,10 +10,29 @@ class ExportSite
     dir = Rails.root.join('tmp', 'exports', @key, @site.slug).to_s
     zip_file = Rails.root.join('tmp', 'exports', @key, "#{@site.slug}.zip").to_s
     FileUtils.mkdir_p(dir)
-    CSV.open("#{dir}/#{@site.slug}.csv", 'w+') do |csv|
+    CSV.open("#{dir}/pages.csv", 'w+') do |csv|
       csv << %w(Title Path Template)
       @site.webpages.each do |page|
         csv << [page.title, page.page_path, page.template.title]
+      end
+    end
+    FileUtils.mkdir_p("#{dir}/resources")
+    @site.resource_types.includes(:resource_fields).each do |resource_type|
+      CSV.open("#{dir}/resources/#{resource_type.slug}.csv", 'w+') do |csv|
+        fields = {}
+        resource_type.resource_fields.each do |field|
+          fields[field.slug] = field.title
+        end
+        csv << ['Page Path'] + fields.values
+        @site.webpages.each do |page|
+          page.resources.where(:resource_type_id => resource_type.id).each do |resource|
+            attrs = [page.page_path]
+            fields.each do |field, label|
+              attrs << resource.send(field)
+            end
+            csv << attrs
+          end
+        end
       end
     end
     @templates.each do |template|
